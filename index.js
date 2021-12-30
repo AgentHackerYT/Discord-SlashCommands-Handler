623bcb276b3b99a7b351eb4f7103cb83bcd3fdfa
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2021-12-30 14:55:30 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js')
+
+const client = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILDS]})
+
+const config = require('./config')
+
+client.on("ready", async() =>{
+
+console.log("Ready")
+
+    let commands = client.application?.commands
+
+   fs.readdir(__dirname + "/commands" , (err , data) =>{
+
+        data.forEach(r => {
+
+        const command = require(`./commands/${r}`)
+
+        commands?.create({name: command.name, description: command.description, options: command.options})
+
+        })
+    
+    })
+
+    
+
+client.on("interactionCreate", (interaction) =>{
+    if(!interaction.isCommand()) return;
+
+    fs.readdir(__dirname + "/commands", (err , data1) =>{
+        data1.forEach(data =>{
+            console.log(data)
+      const command = require(`./commands/${data}`)
+        if(interaction.commandName == command.name){
+                command.run(interaction, client)
+        }
+        })
+        
+    })
+
+  })
+  
+  client.login(config.token)
